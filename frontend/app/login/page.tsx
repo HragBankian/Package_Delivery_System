91bf8ea0736a8d45ed94785baf625919527a1c22
egadb <egadboisroy@gmail.com>
2024-11-13 14:40:27 -0500

    added authjs for sessions with oauth since the db is not up and running yet

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,5 +1,5 @@
-"use client";
 import AuthForm from "@/components/AuthForm";
+import { signIn } from "@/auth";
 const Signup: React.FC = () => {
   const handleLogin = async (data: { email: string; password: string }) => {
     const res = await fetch("/api/auth/login", {
@@ -11,7 +11,15 @@ const Signup: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md w-full max-w-md">
-        <AuthForm mode="Login" onSubmit={handleLogin} />
+        {/* <AuthForm mode="Login" onSubmit={handleLogin} /> */}
+        <form
+          action={async () => {
+            "use server";
+            await signIn("github");
+          }}
+        >
+          <button type="submit">Signin with GitHub</button>
+        </form>
       </div>
     </div>
   );
