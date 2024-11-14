cd9291665d3905323049ea39db434876030d8450
Gregory Demirdjian <gregoryd2003@gmail.com>
2024-11-14 13:18:09 -0500

    Added Observer functions

---
+++ backend/backend/DesignPatternSupportClasses/Observer/IObserver.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.DesignPatternSupportClasses.Observer
+{
+    public interface IObserver
+    {
+        void Update(string trackingStatus, string location);
+    }
+
+    public interface ISubject
+    {
+        void RegisterObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers(Guid trackingNumber);
+    }
+}
+
